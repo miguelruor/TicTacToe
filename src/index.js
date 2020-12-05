fe888b34a8adcb4a3d55c8f706e0dd68a6b8651c
miguelruor <miguelruiz98@outlook.com>
2020-12-04 23:13:14 -0600

    Historial de Tic Tac Toe

--- src/index.js
+++ src/index.js
@@ -77,9 +77,10 @@ class Game extends React.Component {
     }
 
     handleClick(i){
-        const history = this.state.history;
+        const history = this.state.history.slice(0,this.state.stepNumber+1);
         const current = history[history.length-1];
         const squares = current.squares.slice(); //slice copia el array de squares
+
         if(calculateWinner(squares) || squares[i]){
             return;
         }
@@ -89,6 +90,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
@@ -102,19 +104,20 @@ class Game extends React.Component {
 
     render() {
         const history = this.state.history;
-        const current = history[history.length-1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move #' + move : 'Go to start';
-            return(
-                <li key={move}> 
-                    <button onClick = {() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
+            if (move <= this.state.stepNumber){
+
+                const desc = move ? 'Go to move #' + move : 'Go to start';
+                return(
+                    <li key={move}> 
+                        <button onClick = {() => this.jumpTo(move)}>{desc}</button>
+                    </li>
+                );
+            }
         });
-
-
 
         let status;
         if(winner){
